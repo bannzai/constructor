a0a85e31831cf71badcff08acfdfec5dab337775
bannzai <kingkong999yhirose@gmail.com>
2019-05-09 07:57:54 +0900

    :sparkles: Fix input code

--- input/code.go
+++ input/code.go
@@ -1,12 +1,35 @@
 package input
 
-import "github.com/constructor/raw"
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+
+	"github.com/constructor/raw"
+)
 
 type Code interface {
 	Read() raw.Code
 }
-type CodeImpl struct{}
+type CodeImpl struct {
+	FilePath string
+}
 
-func (CodeImpl) Read() raw.Code {
-	return raw.Code{}
+func (impl CodeImpl) Read() raw.Code {
+	buf, err := ioutil.ReadFile(impl.FilePath)
+	if err != nil {
+		panic(err)
+	}
+	fileSet := token.NewFileSet()
+	astFile, err := parser.ParseFile(fileSet, impl.FilePath, buf, 0)
+	fmt.Printf("ast.Visitor = %+v\n", ast.Visitor)
+	if err != nil {
+		panic(err)
+	}
+	return raw.Code{
+		FilePath: impl.FilePath,
+		ASTFile:  astFile,
+	}
 }
--- raw/code.go
+++ raw/code.go
@@ -1,6 +1,9 @@
 package raw
 
+import "go/ast"
+
 // Code is presentation of .go file content.
 type Code struct {
 	FilePath Path
+	ASTFile  ast.File
 }
