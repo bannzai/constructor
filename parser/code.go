86cf0b1f81dfb923fcddcdc33c8da036c6443904
bannzai <kingkong999yhirose@gmail.com>
2019-05-08 22:08:01 +0900

    :sparkles: Prepare parser

--- parser/code.go
+++ parser/code.go
@@ -1,5 +1,15 @@
 package parser
 
+import "github.com/constructor/input"
+
+type CodeParsedResult string
 type Code interface {
+	parse() CodeParsedResult
 }
-type CodeImpl struct{}
+type CodeImpl struct {
+	Input input.Code
+}
+
+func (impl CodeImpl) parse() CodeParsedResult {
+	return ""
+}
--- parser/yaml.go
+++ parser/yaml.go
@@ -1,5 +1,15 @@
 package parser
 
+import "github.com/constructor/input"
+
+type YamlParsedResult string
 type Yaml interface {
+	parse() YamlParsedResult
 }
-type YamlImpl struct{}
+type YamlImpl struct {
+	Input input.Yaml
+}
+
+func (impl CodeImpl) parse() YamlParsedResult {
+	return ""
+}
