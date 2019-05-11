0b9956692f88ec4c551b44f6248471cb57496fe2
bannzai <kingkong999yhirose@gmail.com>
2019-05-11 18:05:44 +0900

    :sparkles: Fix model properties

--- raw/yaml.go
+++ raw/yaml.go
@@ -1,8 +1,15 @@
 package raw
 
+// Definition is each elemnt for yaml
+type Definition struct {
+	Package           string `yaml:"package"`
+	SourcePaths       []Path `yaml:"sourcePaths"`
+	IgnoredPaths      []Path `yaml:"ignoredPaths"`
+	TemplateFilePaths []Path `yaml:"templatePaths"`
+	DestinationPath   Path   `yaml:"destinationPath"`
+}
+
 // Yaml is file data for format of .yaml
 type Yaml struct {
-	Package      string `yaml:"package"`
-	Paths        []Path `yaml:"paths"`
-	IgnoredPaths []Path `yaml:"ignoredPaths"`
+	Definitions []Definition `yaml:"definitions"`
 }
