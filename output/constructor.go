99f4e9ef0de61298c3a8cb0d536afac96e4eed0a
bannzai <kingkong999yhirose@gmail.com>
2019-05-11 18:05:22 +0900

    :sparkles: Fix  constructor for output

--- output/constructor.go
+++ output/constructor.go
@@ -1,10 +1,22 @@
 package output
 
-type Constructor interface {
-	Generate() error
-}
-type ConstructorImpl struct{}
+import (
+	"io/ioutil"
 
-func (ConstructorImpl) Generate() error {
-	return nil
+	"github.com/constructor/model"
+)
+
+type Constructor interface {
+	Generate()
+}
+type ConstructorImpl struct {
+	Source model.GenerateElementEachPackage
+}
+
+func (impl ConstructorImpl) Generate() {
+	source := impl.Source
+	content := source.Content()
+	if err := ioutil.WriteFile(source.DestinationPath, content, 0644); err != nil {
+		panic(err)
+	}
 }
