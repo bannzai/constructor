7c53a65d15978bebc80880c6526e8b60b675e375
bannzai <kingkong999yhirose@gmail.com>
2019-05-13 21:40:35 +0900

    :sparkles: Fix description for generate command

--- cmd/generate.go
+++ cmd/generate.go
@@ -23,13 +23,10 @@ import (
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "generate constructor functions",
+	Long: `When use "generate" command, some constructor functions are generated.
+constructor generate [/path/to/package] [-c(--config) constructor.yaml].
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("generate called")
 	},
