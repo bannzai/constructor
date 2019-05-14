634ee37dc8c57828de8cd16db02d0bd75c09ab17
bannzai <kingkong999yhirose@gmail.com>
2019-05-15 02:05:39 +0900

    :recycle: Instanciate map

--- input/code.go
+++ input/code.go
@@ -36,6 +36,7 @@ func parseASTFile(filePath raw.Path) *ast.File {
 }
 
 func parseASTStructs(file *ast.File) (typeNameAndStruct map[string]*ast.StructType) {
+	typeNameAndStruct = map[string]*ast.StructType{}
 	ast.Inspect(file, func(node ast.Node) bool {
 		lastChildNode := node == nil
 		if lastChildNode {
