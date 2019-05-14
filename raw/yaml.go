4ae70f096d6c2d0c01bf549efb4a906f37c85ef4
bannzai <kingkong999yhirose@gmail.com>
2019-05-15 02:07:32 +0900

    :recycle: Fix define type

--- raw/yaml.go
+++ raw/yaml.go
@@ -1,15 +1,16 @@
 package raw
 
-// Definition is each elemnt for yaml
-type Definition struct {
-	Package           string `yaml:"package"`
-	SourcePaths       []Path `yaml:"sourcePaths"`
-	IgnoredPaths      []Path `yaml:"ignoredPaths"`
-	TemplateFilePaths []Path `yaml:"templatePaths"`
-	DestinationPath   Path   `yaml:"destinationPath"`
-}
-
-// Yaml is file data for format of .yaml
-type Yaml struct {
-	Definitions []Definition `yaml:"definitions"`
-}
+type (
+	// Yaml is file data for format of .yaml
+	Yaml struct {
+		Definitions []Definition `yaml:"definitions"`
+	}
+	// Definition is each elemnt for yaml
+	Definition struct {
+		Package           string `yaml:"package"`
+		SourcePaths       []Path `yaml:"sourcePaths"`
+		IgnoredPaths      []Path `yaml:"ignoredPaths"`
+		TemplateFilePaths []Path `yaml:"templatePaths"`
+		DestinationPath   Path   `yaml:"destinationPath"`
+	}
+)
