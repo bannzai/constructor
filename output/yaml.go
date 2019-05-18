07e81e3f8ae451d8d501af07aaa3870f0183ddae
bannzai <kingkong999yhirose@gmail.com>
2019-05-18 14:52:36 +0900

    :sparkles: Fix output yaml

--- output/yaml.go
+++ output/yaml.go
@@ -1,10 +1,41 @@
 package output
 
+import (
+	"os"
+
+	"github.com/constructor/raw"
+	yaml "gopkg.in/yaml.v2"
+)
+
 type Yaml interface {
-	Setup() error
+	Setup()
 }
 type YamlImpl struct{}
 
-func (YamlImpl) Setup() error {
-	return nil
+const yamlFilePathName = "constructor.yaml"
+
+func (YamlImpl) Setup() {
+	y := raw.Yaml{
+		Definitions: []raw.Definition{
+			raw.Definition{
+				Package:           "",
+				SourcePaths:       []raw.Path{},
+				IgnoredPaths:      []raw.Path{},
+				TemplateFilePaths: []raw.Path{},
+				DestinationPath:   "",
+			},
+		},
+	}
+
+	output, err := yaml.Marshal(y)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create(yamlFilePathName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	file.Write(output)
 }
