c9389a965df012fe5495af46a9996f730fe1372e
bannzai <kingkong999yhirose@gmail.com>
2019-05-18 14:22:58 +0900

    :recycle: Fix template

--- output/template.go
+++ output/template.go
@@ -5,6 +5,30 @@ type Template interface {
 }
 type TemplateImpl struct{}
 
+const defaultTemplate = `
+
+{{- $dot := . -}}
+
+{{range $i, $field := .Structs -}}
+
+{{- $suffix := $dot.Package | upperCamelCase -}}
+{{- $structureName := $dot.Name -}}
+
+// New{{$structureName}}{{.Suffix}} insitanciate {{$dot.Name}}
+func New{{$structureName}}{{$suffix}}(
+	{{range $i, $field := $dot.Fields -}}
+		{{parameterCase $field}} {{$field}},
+	{{end}}
+) {{$structureName}} {
+	return {{$structureName}}{{$suffix}}{
+		{{range $i, $field := $dot.Fields -}}
+			{{$field}}: {{argumentCase $field}},
+		{{end}}
+	}
+}
+{{end}}
+`
+
 func (impl TemplateImpl) Setup() error {
 	return nil
 }
