7a31a9a453073aa8d6023fa0dc82c2d50456479d
bannzai <kingkong999yhirose@gmail.com>
2019-05-19 01:57:26 +0900

    :recycle: Define functions when instanciate template

--- generator/generator.go
+++ generator/generator.go
@@ -14,22 +14,14 @@ type generateComponent struct {
 	DestinationPath raw.Path
 }
 
-var functions = template.FuncMap{
-	"upperCamelCase": upperCamelCase,
-	"paramterCase":   lowerCamelCase,
-	"argumentCase":   lowerCamelCase,
-}
-
 func (g generateComponent) Content() []byte {
-	buf := &bytes.Buffer{}
-	g.Template = g.Template.Funcs(functions)
-
 	structs := []raw.Struct{}
 	for _, sourceCode := range g.SourceCodes {
 		structs = append(structs, sourceCode.Structs...)
 	}
 
-	if err := g.Template.ExecuteTemplate(buf, "constructor", map[string]interface{}{
+	buf := &bytes.Buffer{}
+	if err := g.Template.Execute(buf, map[string]interface{}{
 		"Package": g.Package,
 		"Structs": structs,
 	}); err != nil {
--- reader/template.go
+++ reader/template.go
@@ -2,6 +2,7 @@ package reader
 
 import (
 	"html/template"
+	"strings"
 
 	"github.com/constructor/raw"
 )
@@ -11,6 +12,24 @@ type Template interface {
 }
 type TemplateImpl struct{}
 
+var functions = template.FuncMap{
+	"upperCamelCase": upperCamelCase,
+	"parameterCase":  lowerCamelCase,
+	"argumentCase":   lowerCamelCase,
+}
+
+func lowerCamelCase(target string) string {
+	firstString := strings.ToLower(target[:1])
+	dropedFirstString := target[1:]
+	return firstString + dropedFirstString
+}
+
+func upperCamelCase(target string) string {
+	firstString := strings.ToUpper(target[:1])
+	dropedFirstString := target[1:]
+	return firstString + dropedFirstString
+}
+
 func (impl TemplateImpl) Read(filePath raw.Path) *template.Template {
-	return template.Must(template.New("constructor").ParseFiles(filePath))
+	return template.Must(template.New(filePath).Funcs(functions).ParseFiles(filePath))
 }
