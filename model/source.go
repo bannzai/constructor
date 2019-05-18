b9f6ebe20bfb2871f6d6251de58aedc67304e55b
bannzai <kingkong999yhirose@gmail.com>
2019-05-18 23:22:54 +0900

    :sparkles: Add functions and execute template file

--- model/source.go
+++ model/source.go
@@ -1,6 +1,12 @@
 package model
 
-import "github.com/constructor/raw"
+import (
+	"html/template"
+	"io"
+	"strings"
+
+	"github.com/constructor/raw"
+)
 
 // GenerateElementEachPackage is inter struct for generate constructor.
 type GenerateElementEachPackage struct {
@@ -8,8 +14,35 @@ type GenerateElementEachPackage struct {
 	Template        raw.Template
 	SourceCode      raw.Code
 	DestinationPath raw.Path
+
+	Writer io.Writer
+}
+
+var functions = template.FuncMap{
+	"upperCamelCase": upperCamelCase,
+	"paramterCase":   lowerCamelCase,
+	"argumentCase":   lowerCamelCase,
+}
+
+func lowerCamelCase(target string) string {
+	firstString := strings.ToLower(target[:1])
+	dropedFirstString := target[1:]
+	return firstString + dropedFirstString
+}
+
+func upperCamelCase(target string) string {
+	firstString := strings.ToUpper(target[:1])
+	dropedFirstString := target[1:]
+	return firstString + dropedFirstString
 }
 
 func (t GenerateElementEachPackage) Content() []byte {
+	if err := t.Template.Template.Funcs(functions).Execute(t.Writer, map[string]interface{}{
+		"SourceFilePath": t.SourceCode.FilePath,
+		"Structs":        t.SourceCode.Structs,
+	}); err != nil {
+		panic(err)
+	}
+
 	return []byte{}
 }
