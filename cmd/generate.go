f3b8c5f7e61017c10b24c9c0edc32dc192e6aab6
bannzai <kingkong999yhirose@gmail.com>
2019-05-20 23:05:14 +0900

    :recycle: Remove comments

--- cmd/generate.go
+++ cmd/generate.go
@@ -57,14 +57,4 @@ func generate(yamlFilePath string) {
 func init() {
 	rootCmd.AddCommand(generateCmd)
 	generateCmd.Flags().StringVarP(&generateOptions.yamlFilePath, "configure", "c", raw.YamlFilePathName, "Specify configure file")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// generateCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// generateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/setup.go
+++ cmd/setup.go
@@ -34,14 +34,4 @@ var setupCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(setupCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// setupCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// setupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
