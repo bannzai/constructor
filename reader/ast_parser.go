c52cd0e824b734b4e7d6d6d93684a59b9771a3fb
bannzai <kingkong999yhirose@gmail.com>
2019-05-23 01:12:06 +0900

    :recycle: Move to ast_parser for AST function

---
+++ reader/ast_parser.go
@@ -0,0 +1,48 @@
+package reader
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+
+	"github.com/constructor/structure"
+)
+
+func parseASTFile(filePath structure.Path) *ast.File {
+	buf, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	fileSet := token.NewFileSet()
+	astFile, err := parser.ParseFile(fileSet, filePath, buf, 0)
+	if err != nil {
+		panic(err)
+	}
+	return astFile
+}
+
+func parseASTStructs(file *ast.File) (typeNameAndStruct map[string]*ast.StructType) {
+	typeNameAndStruct = map[string]*ast.StructType{}
+	ast.Inspect(file, func(node ast.Node) bool {
+		lastChildNode := node == nil
+		if lastChildNode {
+			return false
+		}
+
+		typeSpec, ok := node.(*ast.TypeSpec)
+		if !ok {
+			return true
+		}
+
+		name := typeSpec.Name.Name
+		structType, ok := typeSpec.Type.(*ast.StructType)
+		if !ok {
+			return true
+		}
+
+		typeNameAndStruct[name] = structType
+		return true
+	})
+	return
+}
--- reader/code.go
+++ reader/code.go
@@ -3,9 +3,6 @@ package reader
 import (
 	"fmt"
 	"go/ast"
-	"go/parser"
-	"go/token"
-	"io/ioutil"
 	"reflect"
 	"sort"
 	"strings"
@@ -45,44 +42,6 @@ func (impl Code) Read(filePath structure.Path) (code structure.Code) {
 		code.Structs = append(code.Structs, convert(typeName, structure))
 	}
 	code.Structs = sortedStructs(code.Structs)
-	return
-}
-
-func parseASTFile(filePath structure.Path) *ast.File {
-	buf, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		panic(err)
-	}
-	fileSet := token.NewFileSet()
-	astFile, err := parser.ParseFile(fileSet, filePath, buf, 0)
-	if err != nil {
-		panic(err)
-	}
-	return astFile
-}
-
-func parseASTStructs(file *ast.File) (typeNameAndStruct map[string]*ast.StructType) {
-	typeNameAndStruct = map[string]*ast.StructType{}
-	ast.Inspect(file, func(node ast.Node) bool {
-		lastChildNode := node == nil
-		if lastChildNode {
-			return false
-		}
-
-		typeSpec, ok := node.(*ast.TypeSpec)
-		if !ok {
-			return true
-		}
-
-		name := typeSpec.Name.Name
-		structType, ok := typeSpec.Type.(*ast.StructType)
-		if !ok {
-			return true
-		}
-
-		typeNameAndStruct[name] = structType
-		return true
-	})
 	return
 }
 
