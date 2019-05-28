163541b0756f28e6fde5cb2e0d42825622eb3180
bannzai <kingkong999yhirose@gmail.com>
2019-05-29 00:56:47 +0900

    :recycle: Rename function to targetFilePaths

--- generator/glob.go
+++ generator/glob.go
@@ -12,7 +12,7 @@ type FilePathFetcher interface {
 
 type Glob struct{}
 
-func (Glob) sourceFilePaths(definition structure.Definition) []structure.Path {
+func (Glob) targetFilePaths(definition structure.Definition) []structure.Path {
 	filePaths, err := filepath.Glob(definition.SourcePath)
 	if err != nil {
 		panic(err)
