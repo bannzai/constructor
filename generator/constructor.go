d4c8e6ec7ec5d21e69a7a365ca55a6f2a53a5bd6
bannzai <kingkong999yhirose@gmail.com>
2019-09-25 08:20:10 +0900

    :recycle: Remove unnecessary generator function

--- generator/constructor.go
+++ generator/constructor.go
@@ -1,11 +1,39 @@
 package generator
 
+import (
+	"bytes"
+	"html/template"
+
+	"github.com/bannzai/constructor/structure"
+)
+
 type Constructor struct {
 	TemplateReader
 	SourceCodeReader
 	FileWriter
 }
 
-func (impl Constructor) Generate() {
+func (generator Constructor) Generate() {
 
+}
+
+func (generator Constructor) content(
+	packageName string,
+	template *template.Template,
+	sourceCodes []structure.Code,
+	destinationPath structure.Path,
+) string {
+	structs := []structure.Struct{}
+	for _, sourceCode := range sourceCodes {
+		structs = append(structs, sourceCode.Structs...)
+	}
+
+	buf := &bytes.Buffer{}
+	if err := template.Execute(buf, map[string]interface{}{
+		"Package": packageName,
+		"Structs": structs,
+	}); err != nil {
+		panic(err)
+	}
+	return buf.String()
 }
