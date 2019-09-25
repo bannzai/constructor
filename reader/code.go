e61affa3d5ccf4718f5b35058016590b8bec9d6e
bannzai <kingkong999yhirose@gmail.com>
2019-09-26 02:57:16 +0900

    :sparkles: Add read with type function

--- reader/code.go
+++ reader/code.go
@@ -6,9 +6,17 @@ import (
 
 type Code struct{}
 
-func (impl Code) Read(filePath structure.Path) (code structure.Code) {
+func (impl Code) Read(filePath structure.Path) structure.Code {
+	return impl.ReadWithType(filePath, "")
+}
+
+func (impl Code) ReadWithType(filePath structure.Path, generatedTypeName string) (code structure.Code) {
 	code.FilePath = filePath
+	isNotSpecifyType := 0 == len(generatedTypeName)
 	for typeName, structure := range parseASTStructs(parseASTFile(code.FilePath)) {
+		if isNotSpecifyType || typeName != generatedTypeName {
+			continue
+		}
 		code.Structs = append(code.Structs, convert(typeName, structure))
 	}
 	code.Structs = sortedStructs(code.Structs)
