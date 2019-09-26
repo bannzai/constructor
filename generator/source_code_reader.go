ef7b7fc5d5e170d9c1e3e2d97ec9fe4e321c3019
bannzai <kingkong999yhirose@gmail.com>
2019-09-26 11:18:36 +0900

    :memo: Add go generate annotation

--- generator/source_code_reader.go
+++ generator/source_code_reader.go
@@ -1,3 +1,4 @@
+//go:generate mockgen -source=$GOFILE -destination=$GOPATH/src/github.com/bannzai/constructor/generator/source_code_reader_interface_mock_test.go -package=$GOPACKAGE
 package generator
 
 import "github.com/bannzai/constructor/structure"
